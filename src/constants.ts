68dd88c3e0185c56b2ba9c7b650824c7980cd69b
tonybnya <nya.tony2010@gmail.com>
2025-01-22 08:30:55 +0100

    feat: use constants to dynamically handle meta data of the pages like title and description

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const BLOG_TITLE = "TechTonyc Blog";
+export const BLOG_DESCRIPTION =
+  "Articles, Inspiring Stories, and Hands-On Tutorials from the TechTonyc Blog powered by Tony B. NYA, a Software Engineer and ALX/Holberton School Alumni, living at Douala, in Cameroon. ";
